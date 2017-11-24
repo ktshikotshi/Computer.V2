643361f122a58cc5dad8caaaaace62701ae03cae
Khuthadzo Tshikotshi <khuthadzotshikotshi@gmail.com>
2017-11-24 08:42:43 +0200

    make maths class

---
+++ ComputerV2-class/myMaths.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+
+namespace ComputerV2_class
+{
+    public static  class myMaths
+    {
+        public static string calc(string expr)
+        {
+            string[] arr = Regex.Split(expr, @"(\-)|(\+)|(\/)|(\*)|(\%)|(\^)|(\()|(\))");
+            List<string> numbs = new List<string>();
+            foreach (var n in arr)
+            {
+                if (n != "")
+                    numbs.Add(n);
+            }
+            brackets(ref numbs);
+            power(ref numbs);
+            multDiv(ref numbs);
+            for (var i = 0; i < numbs.Count; i++)
+            {
+                if (numbs[i] == "+" )
+                {
+                    var n = Convert.ToDouble(numbs[i - 1]) + Convert.ToDouble(numbs[i + 1]);
+                    numbs[i - 1] = n.ToString();
+                    numbs.RemoveRange(i, 2);
+                    i = 0;
+                }
+                else if (numbs[i] == "-")
+                {
+                    var n = Convert.ToDouble(numbs[i - 1]) - Convert.ToDouble(numbs[i + 1]);
+                    numbs[i - 1] = n.ToString();
+                    numbs.RemoveRange(i, 2);
+                    i = 0;
+                }
+            }
+            return numbs[0];
+        }
+        public static void brackets(ref List<string> expr)
+        {
+            int countBraces = 0;
+            bool fBrace = false;
+            int[] braceLoc = { 0, 0 };
+            for(var i = 0; i < expr.Count; i++)
+            {
+                if (expr[i] == "(")
+                {
+                    if (countBraces == 0)
+                        braceLoc[0] = i + 1;
+                    countBraces += 1;
+                    fBrace = true;
+                }
+                else if (expr[i] == ")")
+                {
+                    countBraces -= 1;
+                    if (fBrace == true && countBraces == 0)
+                    {
+                        braceLoc[1] = i;
+                    }
+                }
+                if (fBrace == true && countBraces == 0)
+                {
+                    string tmp = "";
+                    for(var j = braceLoc[0]; j < braceLoc[1]; j++)
+                    {
+                        tmp += expr[j];
+                    }
+                    tmp = calc(tmp);
+                    expr[braceLoc[0] - 1] = tmp;
+                    expr.RemoveRange(braceLoc[0], (braceLoc[1] - braceLoc[0]) + 1);
+                    i = 0;
+                    fBrace = false;
+                }
+            }
+        }
+        public  static void power(ref List<string> expr)
+        {
+            double pow = 0, val = 0;
+            for (var j = 0; j < expr.Count; j++)
+            {
+                if (expr[j] == "^")
+                {
+                    pow = Convert.ToDouble(expr[j + 1]);
+                    val = Convert.ToDouble(expr[j - 1]);
+                    double output = pow > 0? val : 0;
+                    for (var i = 1; i < pow; i++)
+                        output *= (val*i);
+                    expr[j - 1] = output.ToString();
+                    expr.RemoveRange(j, 2);
+                    j = 0;
+                }
+            }
+        }
+        public static void multDiv(ref List<string> numbs)
+        {
+            for (var i = 0; i < numbs.Count; i++)
+            {
+                if (numbs[i] == "/")
+                {
+                    var n = Convert.ToDouble(numbs[i - 1]) / Convert.ToDouble(numbs[i + 1]);
+                    numbs[i - 1] = n.ToString();
+                    numbs.RemoveRange(i, 2);
+                    i = 0;
+                }
+                else if (numbs[i] == "*")
+                {
+                    var n = Convert.ToDouble(numbs[i - 1]) * Convert.ToDouble(numbs[i + 1]);
+                    numbs[i - 1] = n.ToString();
+                    numbs.RemoveRange(i, 2);
+                    i = 0;
+                }
+                else if (numbs[i] == "%")
+                {
+                    var n = Convert.ToDouble(numbs[i - 1]) % Convert.ToDouble(numbs[i + 1]);
+                    numbs[i - 1] = n.ToString();
+                    numbs.RemoveRange(i, 2);
+                    i = 0;
+                }
+            }
+        }
+    }
+}
--- ComputerV2/Program.cs
+++ ComputerV2/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections;
 using System.Text.RegularExpressions;
-
+using ComputerV2_class;
 namespace ComputerV2
 {
     internal class Program
@@ -80,7 +79,7 @@ namespace ComputerV2
                                     str += s;
                                 //replace variables with values
                                 if (!(Regex.IsMatch(str, @"[a-zA-Z]+")))
-                                    sTmp[1] = calc(str);
+                                    sTmp[1] = myMaths.calc(str);
                                 else
                                     sTmp[1] = str;
                             }
@@ -170,125 +169,13 @@ namespace ComputerV2
                                 str += s;
                             }
                             //replace variables with values
-                            Console.WriteLine(calc(str));
+                            Console.WriteLine(myMaths.calc(str));
                         }
                     }
                 }
             }
         }
-        public static string calc(string expr)
-        {
-            string[] arr = Regex.Split(expr, @"(\-)|(\+)|(\/)|(\*)|(\%)|(\^)|(\()|(\))");
-            List<string> numbs = new List<string>();
-            foreach (var n in arr)
-            {
-                if (n != "")
-                    numbs.Add(n);
-            }
-            brackets(ref numbs);
-            power(ref numbs);
-            multDiv(ref numbs);
-            for (var i = 0; i < numbs.Count; i++)
-            {
-                if (numbs[i] == "+" )
-                {
-                    var n = Convert.ToDouble(numbs[i - 1]) + Convert.ToDouble(numbs[i + 1]);
-                    numbs[i - 1] = n.ToString();
-                    numbs.RemoveRange(i, 2);
-                    i = 0;
-                }
-                else if (numbs[i] == "-")
-                {
-                    var n = Convert.ToDouble(numbs[i - 1]) - Convert.ToDouble(numbs[i + 1]);
-                    numbs[i - 1] = n.ToString();
-                    numbs.RemoveRange(i, 2);
-                    i = 0;
-                }
-            }
-            return numbs[0];
-        }
-        public static void brackets(ref List<string> expr)
-        {
-            int countBraces = 0;
-            bool fBrace = false;
-            int[] braceLoc = { 0, 0 };
-            for(var i = 0; i < expr.Count; i++)
-            {
-                if (expr[i] == "(")
-                {
-                    if (countBraces == 0)
-                        braceLoc[0] = i + 1;
-                    countBraces += 1;
-                    fBrace = true;
-                }
-                else if (expr[i] == ")")
-                {
-                    countBraces -= 1;
-                    if (fBrace == true && countBraces == 0)
-                    {
-                        braceLoc[1] = i;
-                    }
-                }
-                if (fBrace == true && countBraces == 0)
-                {
-                    string tmp = "";
-                    for(var j = braceLoc[0]; j < braceLoc[1]; j++)
-                    {
-                        tmp += expr[j];
-                    }
-                    tmp = calc(tmp);
-                    expr[braceLoc[0] - 1] = tmp;
-                    expr.RemoveRange(braceLoc[0], (braceLoc[1] - braceLoc[0]) + 1);
-                    i = 0;
-                    fBrace = false;
-                }
-            }
-        }
-        public  static void power(ref List<string> expr)
-        {
-            double pow = 0, val = 0;
-            for (var j = 0; j < expr.Count; j++)
-            {
-                if (expr[j] == "^")
-                {
-                    pow = Convert.ToDouble(expr[j + 1]);
-                    val = Convert.ToDouble(expr[j - 1]);
-                    double output = pow > 0? val : 0;
-                    for (var i = 1; i < pow; i++)
-                        output *= (val*i);
-                    expr[j - 1] = output.ToString();
-                    expr.RemoveRange(j, 2);
-                    j = 0;
-                }
-            }
-        }
-        public static void multDiv(ref List<string> numbs)
-        {
-            for (var i = 0; i < numbs.Count; i++)
-            {
-                if (numbs[i] == "/")
-                {
-                    var n = Convert.ToDouble(numbs[i - 1]) / Convert.ToDouble(numbs[i + 1]);
-                    numbs[i - 1] = n.ToString();
-                    numbs.RemoveRange(i, 2);
-                    i = 0;
-                }
-                else if (numbs[i] == "*")
-                {
-                    var n = Convert.ToDouble(numbs[i - 1]) * Convert.ToDouble(numbs[i + 1]);
-                    numbs[i - 1] = n.ToString();
-                    numbs.RemoveRange(i, 2);
-                    i = 0;
-                }
-                else if (numbs[i] == "%")
-                {
-                    var n = Convert.ToDouble(numbs[i - 1]) % Convert.ToDouble(numbs[i + 1]);
-                    numbs[i - 1] = n.ToString();
-                    numbs.RemoveRange(i, 2);
-                    i = 0;
-                }
-            }
-        }
+        
         public static int findVar(List<List<string>> variables, string str)
         {
             List<string> cVar = new List<string>();
