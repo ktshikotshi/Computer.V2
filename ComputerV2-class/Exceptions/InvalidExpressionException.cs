6db90c7718550c0fbdd1f19c1de51712b3660a07
Khuthadzo Tshikotshi <khuthadzotshikotshi@gmail.com>
2017-12-01 19:08:24 +0200

    new Maths class

---
+++ ComputerV2-class/Exceptions/InvalidExpressionException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace ComputerV2_class.Exceptions
+{
+    class InvalidExpressionException : Exception
+    {
+        public InvalidExpressionException(string message) : base(message) { }
+    }
+}
---
+++ ComputerV2-class/Maths.cs
@@ -0,0 +1,104 @@
+﻿using System.Text.RegularExpressions;
+using ComputerV2_class.Exceptions;
+namespace ComputerV2_class
+{
+    public class Maths
+    {
+        //throws InvalidExpressionException
+        public static string Calculate(string expression)
+        {
+            Brackets(ref expression);
+            Pow(ref expression);
+            DivideMultiply(ref expression);
+            Addition(ref expression);
+            return (expression);
+        }
+
+        private static void Brackets(ref string expression)
+        {
+            var regex = new Regex(@"(?<=\()(.*)(?=\))");
+            while (regex.IsMatch(expression))
+            {
+                var match = regex.Match(expression).Value;
+                expression = expression.Replace ($"({match})", Calculate(match));
+            }
+        }
+
+        private static void Pow(ref string expression)
+        {
+            var regex = new Regex(@"((\-)?\d+([\.]\d+)?)\^((\-)?\d+([\.]\d+)?)");
+            while (regex.IsMatch(expression))
+            {
+                var match = regex.Match(expression).Value;
+                match = ReplaceDecimalPoint(match);
+                var exp = match.Split('^');
+                try
+                {
+                    double value = double.Parse(exp[0]);
+                    //will throw formateException if number is not whole and positive.
+                    int count = int.Parse(exp[1]);
+                    if (count < 0)
+                        throw new System.FormatException();
+                    double res = value;
+                    for (var i = 1; i < count; i++) res *= value;
+                    expression = expression.Replace(match, ReplaceDecimalPoint(res.ToString("0." + new string('#', 9999))));
+                }
+                catch(System.FormatException)
+                {
+                    throw new InvalidExpressionException("Power is of wrong format");
+                }              
+            }
+        }
+
+        private static void DivideMultiply(ref string expression)
+        {
+            var regex = new Regex(@"((\-)?\d+([\.]\d+)?)(\*|\%|\/)((\-)?\d+([\.]\d+)?)");
+            while(regex.IsMatch(expression))
+            {
+                var match = regex.Match(expression).Value;
+                match = ReplaceDecimalPoint(match);
+                var exp = Regex.Split(match, @"(\*)|(\/)|(\%)");
+                double n1 = double.Parse(exp[0]), n2 = double.Parse(exp[2]), res = 0;
+                switch (exp[1])
+                {
+                    case "*" : res = n1 * n2; break;
+                    case "%" : res = n1 % n2; break;
+                    default  : res = n1 / n2; break;
+                }
+                expression = expression.Replace(match, ReplaceDecimalPoint($"{res}"));
+            }
+        }
+
+        private static void Addition(ref string expression)
+        {
+            var regex = new Regex(@"((\-)?\d+([\.]\d+)?)\+((\-)?\d+([\.]\d+)?)");
+            expression = ManageNegative(expression);
+            while(regex.IsMatch(expression))
+            {
+                var match = regex.Match(expression).Value;
+                match = ReplaceDecimalPoint(match);
+                var exp = Regex.Split(match, @"\+");
+                double n1 = double.Parse(exp[0]), n2 = double.Parse(exp[1]), res = 0;
+                res = n1 + n2;
+                expression = expression.Replace(match, ReplaceDecimalPoint($"{res}"));
+            }
+        }
+
+        private static string ManageNegative(string expression)
+        {
+            var regex = new Regex(@"(\d+([\.]\d+)?)(\-)(\d+([\.]\d+)?)");
+            while (regex.IsMatch(expression))
+                expression = regex.Replace(expression, "$1+$3$4");
+            return (expression);
+        }
+
+        private static string ReplaceDecimalPoint(string expression)
+        {
+            if (Regex.IsMatch(expression, @"([\.])(\d+)"))
+                expression = Regex.Replace(expression, @"([\.])(\d+)", ",$2");
+            else if (Regex.IsMatch(expression, @"([\,])(\d+)"))
+                expression = Regex.Replace(expression, @"([\,])(\d+)", ".$2");
+            return (expression);
+        }
+    }
+}
