1dba7427d577e87f49f0b6c0d06b7e6d3e845d71
Khuthadzo Tshikotshi <khuthadzotshikotshi@gmail.com>
2017-12-02 11:10:15 +0200

    remove degree function from Maths class

--- ComputerV2-class/Maths.cs
+++ ComputerV2-class/Maths.cs
@@ -11,7 +11,6 @@ namespace ComputerV2_class
             Brackets(ref expression);
             Pow(ref expression);
             DivideMultiply(ref expression);
-            //Degree(ref expression);
             Imaginary(ref expression);
             Addition(ref expression);
             return (expression);
@@ -80,35 +79,6 @@ namespace ComputerV2_class
             }
         }
 
-        private static void Degree(ref string expression)
-        {
-            var regex = new Regex(@"(\d+([\.]\d+)?)?(\*)?[A-Za-z]+(\^(\d+([\.]\d+)?))?");
-            var newExpression = "";
-            if (regex.IsMatch(expression))
-            {
-                var dgr = FindLargestDegree(expression);
-                if (dgr.BiggestD > -1) {
-                    for (int i = 0; i <= dgr.BiggestD; i++)
-                    {
-                        var res = 0d;
-                        var rgxString = @"(\d+([\.]\d+)?)?((\*)?[A-Za-z]+)(\^(["+ i +"]))";
-                        var rgx = new Regex(rgxString);
-                        while (rgx.IsMatch(expression))
-                        {
-                            var match = rgx.Match(expression).Value;
-                            var nmb = Regex.Match(match, @"((\-|\+)?\d+([\.]\d+)?)").Value;
-                            res += double.Parse(nmb);
-                            expression = expression.Replace(match, "");
-                        }
-                        if (res != 0)
-                            if (i > 0)
-                                newExpression = $"{res}*{dgr.Var}{(i > 1 ? "^" + i : "")}+0" + newExpression;
-                    }
-                }
-                expression = newExpression + expression;
-            }
-        }
-
         private static void Imaginary(ref string expression)
         {
             var regex = new Regex(@"((\-|\+)?\d+([\.]\d+)?)(\*)?i");
@@ -158,33 +128,6 @@ namespace ComputerV2_class
             else if (Regex.IsMatch(expression, @"([\,])(\d+)"))
                 expression = Regex.Replace(expression, @"([\,])(\d+)", ".$2");
             return (expression);
-        }
-        private static (int BiggestD, string Var) FindLargestDegree(string expression)
-        {
-            var regex = new Regex(@"(\d+([\.]\d+)?)?(\*)?[A-Za-z]+(\^(\d+([\.]\d+)?))?");
-            if (regex.IsMatch(expression))
-            {
-                var var = Regex.Match(expression, @"(?<=(\d+([\.]\d+)?)?(\*)?)([A-Za-z]+)(?=((\^)(\d+([\.]\d+)?)?))");
-                var matches = Regex.Matches(expression, @"(?<=(\d+([\.]\d+)?)?(\*)?[A-Za-z]+(\^))(\d+([\.]\d+)?)?");
-                var dgree = 0;
-                for (var i = 0;i < matches.Count; i++)
-                {
-                    try
-                    {
-                        var numb = int.Parse(matches[i].Value);
-                        if (numb < 0)
-                            throw new System.FormatException();
-                        if (numb > dgree)
-                            dgree = numb;
-                    }
-                    catch(System.FormatException)
-                    {
-                        throw new InvalidExpressionException($"Power is not Positive and Whole : {matches[i].Value}");
-                    }
-                }
-                return (dgree, var.Value);
-            }
-            return (-1, null);
         }
     }
 }
