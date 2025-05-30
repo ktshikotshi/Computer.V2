0eb03a099960fc08b597478e4d30bf64c40c175c
Khuthadzo Tshikotshi <khuthadzot@payspace.com>
2025-05-30 14:05:50 +0200

    Update DummyClass.cs

--- src/Computer.V2.Lib/DummyClass.cs
+++ src/Computer.V2.Lib/DummyClass.cs
@@ -1,11 +1,14 @@
 namespace Computer.V2.Lib;
 
 public class DummyClass {
-    const int SOME_NUMBER = 1;
+    private const int SOME_NUMBER = 1;
+    private const int SOME_OTHER_NUMBER = 2;
     public bool ShouldDoSomething(int someNumber)
     {
-        if (someNumber == SOME_NUMBER) return true;
-
-        return false;
+        var someString = string.Empty;
+        if (someNumber == SOME_NUMBER) someString = "1";
+        if (someNumber == SOME_OTHER_NUMBER) someString = "2";
+        
+        return someString == "1";
     }
 }
